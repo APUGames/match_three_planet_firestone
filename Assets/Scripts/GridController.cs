91dadaaa027d5727dd0b5c6d3d7c64f8ad5a1056
Firestone <112723894+Ghost-Beans@users.noreply.github.com>
2023-03-27 19:37:27 -0700

    Planets Randomize

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -1,11 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+
 
 public class GridController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject piecePrefab;
+    private GameObject piecePrefabBlue;
+    [SerializeField]
+    private GameObject piecePrefabPurp;
+    [SerializeField]
+    private GameObject piecePrefabPink;
+    [SerializeField]
+    private GameObject piecePrefabGreen;
+    [SerializeField]
+    private GameObject piecePrefabYellow;
+    [SerializeField]
+    private GameObject piecePrefabOrange;
+    //instandiating for differnt positions of prefabs does not work unfortunately
+
 
     [SerializeField]
     private Vector3 originPosition;
@@ -23,10 +35,46 @@ public class GridController : MonoBehaviour
                 Vector3 newWorldPosition = new Vector3(originPosition.x + row, originPosition.y, originPosition.z - column);
 
                 grid[row, column] = new Piece(newWorldPosition, new Vector2(row, column));
-            
-               Instantiate(piecePrefab, grid[row, column].GetPosition(), Quaternion.identity);
+
+                System.Random rand = new System.Random();
+
+                int randomNum = rand.Next(1, 90);
+
+                if(randomNum >= 1 && randomNum < 15)
+                {
+                    Instantiate(piecePrefabBlue, grid[row, column].GetPosition(), Quaternion.identity);
+                }
+                else if (randomNum >= 15 && randomNum < 30)
+                {
+                    Instantiate(piecePrefabPurp, grid[row, column].GetPosition(), Quaternion.identity);
+
+                }
+                else if (randomNum >= 30 && randomNum < 45)
+                {
+                    Instantiate(piecePrefabPink, grid[row, column].GetPosition(), Quaternion.identity);
+
+                }
+                else if (randomNum >= 45 && randomNum < 60)
+                {
+                    Instantiate(piecePrefabGreen, grid[row, column].GetPosition(), Quaternion.identity);
+
+                }
+                else if (randomNum >= 60 && randomNum < 75)
+                {
+                    Instantiate(piecePrefabYellow, grid[row, column].GetPosition(), Quaternion.identity);
+
+                }
+                else if (randomNum >= 75 && randomNum < 90)
+                {
+                    Instantiate(piecePrefabOrange, grid[row, column].GetPosition(), Quaternion.identity);
+
+                }
+
+
             }
+
         }
+
     }
 
     // Update is called once per frame
